a7ed5f669ac58987629e4dc9fff3fbe8b5060500
laura.malagon021 <lauris142003@gmail.com>
2023-05-23 11:11:29 -0500

    persistence commit

--- src/co/edu/uptc/properties/PropertiesManager.java
+++ src/co/edu/uptc/properties/PropertiesManager.java
@@ -14,5 +14,7 @@ public class PropertiesManager {
         messages.load(new FileReader("data/messages.properties"));
     }
 
-
+    public String jsonRoute(){
+        return messages.getProperty("hotelRoute");
+    }
 }
